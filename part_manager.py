f5fe013f270d56cfdc0d43006d4064ef7b4d5207
YunwooC <69556101+YunwooC@users.noreply.github.com>
2021-07-19 13:46:20 -0700

    Update part_manager.py

--- part_manager.py
+++ part_manager.py
@@ -1,23 +1,28 @@
 import tkinter as tk
-from tkinter import *
 from tkinter import ttk
-from tkinter.ttk import *
 
 # main window
 window = tk.Tk()
 window.title('Growing Degrees Day Simulator')   
 window.geometry('800x800')
 
+# row configuration
 rows = 0
 while rows < 50:
     window.rowconfigure(rows, weight=1)
     window.columnconfigure(rows, weight=1)
     rows += 1
 
+# style
+style = ttk.Style()
+style.configure("TFrame", background="#fff")
+    
+# parent tab
 tabControl = ttk.Notebook(window)
 tabControl.grid(row=10,column=0, columnspan=50, rowspan=49)
 tab1 = ttk.Frame(tabControl)
 tab2 = ttk.Frame(tabControl)
+
 # two tabs
 tabControl.add(tab1, text ='Graph')
 tabControl.add(tab2, text ='How To use')
@@ -35,16 +40,16 @@ ttk.Label(tab2,
                                     pady = 30)
 
 
-date = Spinbox(tab1, from_=1, to=31, width=5).grid(row=11,column=10)
+date = ttk.Spinbox(tab1, from_=1, to=31, width=5).grid(row=11,column=10)
 tk.Label(tab1,text="date").grid(row=11,column=16)
-month = Spinbox(tab1, from_=1, to=12, width=5).grid(row=11,column=21)
+month = ttk.Spinbox(tab1, from_=1, to=12, width=5).grid(row=11,column=21)
 tk.Label(tab1,text="Month").grid(row=11,column=28)
 
 array=[]
 for i in range(1970,2023,1):
     array.append(i)
 
-combo = Combobox(tab1,width=5)
+combo = ttk.Combobox(tab1,width=5)
 combo['values']=array
 combo.current(0)
 combo.grid(row=11, column=33)
