934eaa8861898b3f34c111d3065fd98dd4a4ef61
Ruby Chen <ruby_ch22@outlook.com>
2021-07-23 20:27:30 -0400

    reposition and layout

--- part_manager.py
+++ part_manager.py
@@ -6,11 +6,11 @@ from tkinter import messagebox
 # main window
 window = tk.Tk()
 window.title('Growing Degrees Day Simulator')   
-window.geometry('800x800')
+window.geometry('800x600')
 
 # row configuration
 rows = 0
-while rows < 50:
+while rows < 40:
     window.rowconfigure(rows, weight=1)
     window.columnconfigure(rows, weight=1)
     rows += 1
@@ -49,12 +49,12 @@ ttk.Label(tab2,
 # Graph Placement
 # a canvas for placeholder. Modify to reflect the design
 canvas = tk.Canvas(tab1,width=400,height=350)
-canvas.place(x=45, y=360)
+canvas.place(x=30, y=100)
 
 #location section
-loc = tk.Label(tab1, text="Location").place(x=500, y=160)
-location = tk.Button(tab1, text="Location", width=10, height=2).place(x=500, y=185)
-map_button = tk.Button(tab1, text="Map", width=10, height=2).place(x=500, y=225)
+loc = tk.Label(tab1, text="Location").place(x=500, y=180)
+location = tk.Button(tab1, text="Location", width=10, height=2).place(x=500, y=210)
+map_button = tk.Button(tab1, text="Map", width=10, height=2).place(x=500, y=250)
 
 # date selection
 planting_date = tk.Label(tab1, text="Planting Date").place(x=500, y=100)
@@ -77,20 +77,20 @@ tk.Label(tab1,text="Year").place(x=730, y=130)
 # temperature selection
 def update_temp(value=None):
     text = f'{slider.get()} / 50'
-    tk.Label(tab1, text=text).place(x=700, y=470)
+    tk.Label(tab1, text=text).place(x=600, y=360)
 
-tk.Label(tab1, text='30/50').place(x=700, y=470)    # for default value
-tk.Label(tab1, text="Base Temperature (F)").place(x=600, y=400)
+tk.Label(tab1, text='30/50').place(x=500, y=360)    # for default value
+tk.Label(tab1, text="Base Temperature (F)").place(x=500, y=330)
 # command updates the value as slider toggles left or right
 slider = tk.Scale(tab1, from_=30, to=50, orient=tk.HORIZONTAL, showvalue=0, command=update_temp)
-slider.place(x=600, y=470)
+slider.place(x=500, y=360)
 
 #open the info box
 def onClick():
     tk.messagebox.showinfo("What is GDD?",  "In the absence of extreme conditions such as unseasonal drought or disease,"
  " plants grow in a cumulative stepwise manner which is strongly influenced by the ambient temperature."
  " Growing degree days take aspects of local weather into account and allow gardeners to predict "
- " (or, in greenhouses, even to control) the plants' pace toward maturity.")
+ " (or, in greenhouses, even to control) the plants' pace toward maturity. source:wikipedia")
 
 #info button
 infobutton = tk.Button(window, text="More Info", command=onClick, height=2, width=10)
