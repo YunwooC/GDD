2e6d775a560c3e614de4cffee10b6ef527eb5c79
YunwooC <69556101+YunwooC@users.noreply.github.com>
2021-07-26 15:15:22 -0700

    Layout change

--- part_manager.py
+++ part_manager.py
@@ -5,7 +5,8 @@ from tkinter import messagebox
 
 # main window
 window = tk.Tk()
-window.title('Growing Degrees Day Simulator')   
+window.title('Growing Degrees Day Simulator')
+window.configure(bg='white')
 window.geometry('800x600')
 
 # row configuration
@@ -17,16 +18,16 @@ while rows < 40:
 
 # style
 style = ttk.Style()
-style.configure("TNotebook", background="grey")     # Just to make it easier to see.
-style.configure("TFrame", background="#fff")
+style.configure("TNotebook", background="white")
+style.configure("TFrame", background="white")
+style.configure("TLabel", foreground="#254647", background="white")
 
 #title
-title=ttk.Label(window, text ="Growing Degree Day Simulator",font=("Arial Bold",30))
+title=ttk.Label(window, text ="Growing Degree Day Simulator",font=("Arial Bold",30), padding=20)
 title.pack(side='top')
 
 # parent tab
 tabControl = ttk.Notebook(window)
-# tabControl.grid(row=10, column=0, columnspan=50, rowspan=49)
 tab1 = ttk.Frame(tabControl)
 tab2 = ttk.Frame(tabControl)
 
@@ -36,10 +37,7 @@ tabControl.add(tab2, text ='How To use')
 tabControl.pack(expand = 1, fill ="both")
 
 ttk.Label(tab1, 
-    text ="GDD Graph",font=("Arial Bold",30)).grid(column = 0, 
-                               row = 10,
-                               padx = 30,
-                               pady = 30)  
+    text ="GDD Graph",font=("Arial Bold",30)).place(x=30, y=20)
 ttk.Label(tab2,
     text ="Tutorial",font=("Arial Bold",30)).grid(column = 0,
                                     row = 10, 
@@ -48,20 +46,15 @@ ttk.Label(tab2,
 
 # Graph Placement
 # a canvas for placeholder. Modify to reflect the design
-canvas = tk.Canvas(tab1,width=400,height=350)
-canvas.place(x=30, y=100)
-
-#location section
-loc = tk.Label(tab1, text="Location").place(x=500, y=180)
-location = tk.Button(tab1, text="Location", width=10, height=2).place(x=500, y=210)
-map_button = tk.Button(tab1, text="Map", width=10, height=2).place(x=500, y=250)
+canvas = tk.Canvas(tab1, width=500,height=320)
+canvas.place(x=30, y=90)
 
 # date selection
-planting_date = tk.Label(tab1, text="Planting Date").place(x=500, y=100)
-date = ttk.Spinbox(tab1, from_=1, to=31, width=5).place(x=500, y=130)
-tk.Label(tab1,text="date").place(x=550,y=130)
-month = ttk.Spinbox(tab1, from_=1, to=12, width=5).place(x=580,y=130)
-tk.Label(tab1,text="Month").place(x=630,y=130)
+ttk.Label(tab1, text="Planting Date", font=(14)).place(x=560, y=100)
+date = ttk.Spinbox(tab1, from_=1, to=31, width=5).place(x=560, y=152)
+ttk.Label(tab1,text="date").place(x=560,y=130)
+month = ttk.Spinbox(tab1, from_=1, to=12, width=5).place(x=623,y=152)
+ttk.Label(tab1,text="Month").place(x=623,y=130)
 
 array=[]
 for i in range(1970,2023,1):
@@ -70,20 +63,26 @@ for i in range(1970,2023,1):
 combo = ttk.Combobox(tab1,width=5)
 combo['values']=array
 combo.current(0)
-combo.place(x=673, y=130)
+combo.place(x=686, y=152)
 
-tk.Label(tab1,text="Year").place(x=730, y=130)
+ttk.Label(tab1,text="Year").place(x=686, y=130)
+
+#location section
+ttk.Label(tab1, text="Location", font=(14)).place(x=560, y=210)
+location = ttk.Button(tab1, text="Location", width=25).place(x=560, y=242)
+map_button = ttk.Button(tab1, text="Map", width=25).place(x=560, y=275)
 
 # temperature selection
 def update_temp(value=None):
-    text = f'{slider.get()} / 50'
-    tk.Label(tab1, text=text).place(x=600, y=360)
+    text = f'{int(slider.get())}'
+    ttk.Label(tab1, text=text).place(x=707, y=370)
 
-tk.Label(tab1, text='30/50').place(x=500, y=360)    # for default value
-tk.Label(tab1, text="Base Temperature (F)").place(x=500, y=330)
+
+ttk.Label(tab1, text="Base Temperature (F)", font=(14)).place(x=560, y=330)
 # command updates the value as slider toggles left or right
-slider = tk.Scale(tab1, from_=30, to=50, orient=tk.HORIZONTAL, showvalue=0, command=update_temp)
-slider.place(x=500, y=360)
+slider = ttk.Scale(tab1, from_=30, to=50, orient=tk.HORIZONTAL, length=136, command=update_temp)
+slider.place(x=560, y=370)
+ttk.Label(tab1, text='30 / 50').place(x=707, y=370)    # for default value
 
 #open the info box
 def onClick():
