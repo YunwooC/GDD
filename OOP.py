ed0d2b5009e495a28c99e6b8553a67c06bfd141b
YunwooC <69556101+YunwooC@users.noreply.github.com>
2021-07-29 19:05:30 -0700

    Update OOP.py
    part of this is copy-paste from the example code and some of the View() code is ours! example code for Model() and Controller() should be helpful for fetching graph.

--- OOP.py
+++ OOP.py
@@ -63,24 +63,6 @@ class View():
 
         ttk.Label(self.tab1,text="Year",font=("Arial",11)).place(x=686, y=130)
 
-        # self.fig = Figure(figsize=(7.5, 4), dpi=80)
-        # self.ax0 = self.fig.add_axes((0.05, .05, .90, .90), axisbg=(.75, .75, .75), frameon=False)
-        # self.frame.pack(side=Tk.LEFT, fill=Tk.BOTH, expand=1)
-        # self.sidepanel = SidePanel(master)
-        # self.canvas = FigureCanvasTkAgg(self.fig, master=self.frame)
-        # self.canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
-        # self.canvas.show()
-
-
-# class SidePanel():
-#     def __init__(self, root):
-#         self.frame2 = Tk.Frame(root)
-#         self.frame2.pack(side=Tk.LEFT, fill=Tk.BOTH, expand=1)
-#         self.plotBut = Tk.Button(self.frame2, text="Plot ")
-#         self.plotBut.pack(side="top", fill=Tk.BOTH)
-#         self.clearButton = Tk.Button(self.frame2, text="Clear")
-#         self.clearButton.pack(side="top", fill=Tk.BOTH)
-
 
 class Controller():
     def __init__(self):
